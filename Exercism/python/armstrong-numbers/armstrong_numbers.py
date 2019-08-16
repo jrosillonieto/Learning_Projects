cd40f3cccfba71e2828c9fdb92e44784d7b7ce52
unknown <jrosillonieto@gmail.com>
2019-08-16 23:41:56 +0200

    Armstrong Exercise

--- Exercism/python/armstrong-numbers/armstrong_numbers.py
+++ Exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,9 @@
 def is_armstrong_number(number):
-    pass
+    number_str = str(number)
+    number_len = len(number_str)
+    sum = 0
+    for digit in number_str:
+        sum = sum + int(digit)**(int(number_len))
+        if sum == number:
+            return True
+    return False
